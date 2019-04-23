eb6e0ca5a136525fa2a5a2c9953f55e314301493
justinposer <jposer1@gmail.com>
2019-04-23 15:38:15 -0400

    react basics added and tested

---
+++ client/src/app.jsx
@@ -0,0 +1,25 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+// import axios from "axios";
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+
+  }
+
+  
+
+  render() {
+    return (
+      <div>
+          <h1> test successful</h1>
+      </div>    
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
