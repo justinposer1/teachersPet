d3f0d0f50cb1f133638eaf191795455ccc302cda
justinposer <jposer1@gmail.com>
2019-04-23 17:36:23 -0400

    found and installed styling package

---
+++ client/src/modules/login.jsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+        
+  
+    }
+  
+    render() {
+      return (
+        <div>
+            <h1> test successful</h1>
+        </div>    
+      )
+    }
+  }
