583a81c07aaa7da2e360bfeb126127d29aba6e12
justinposer <jposer1@gmail.com>
2019-04-29 18:27:46 -0400

    changed planning logic of account creation

--- client/src/modules/login.jsx
+++ client/src/modules/login.jsx
@@ -1,76 +1,89 @@
-
 import React from "react";
-import { Input, Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
+import {
+    Icon,
+  Input,
+  Button,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment
+} from "semantic-ui-react";
 
 class Login extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-          input: '',
-          displayed: 'nothing'
-      }
-      this.changeInput = this.changeInput.bind(this);
-      this.changeDisplayed = this.changeDisplayed.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
+    this.changeInput = this.changeInput.bind(this);
+    this.changeDisplayed = this.changeDisplayed.bind(this);
+  }
 
-    changeInput(e) {
-        this.setState({
-            input: e.target.value
-        })
-    }
+  changeInput(e) {
+    this.setState({
+      input: e.target.value
+    });
+  }
 
-    changeDisplayed() {
-        this.setState({
-            displayed: this.state.input
-        })
-    }
+  changeDisplayed() {
+    this.setState({
+      displayed: this.state.input
+    });
+  }
 
-    render() {
-      return (
-        <div className='login-form'>
-        
-        <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+  render() {
+    return (
+      <div className="login-form">
+        <Grid
+          textAlign="center"
+          style={{ height: "100%", marginTop: "3em" }}
+          verticalAlign="middle"
+        >
           <Grid.Column style={{ maxWidth: 450 }}>
-            <Header as='h2' color='teal' textAlign='center'>
-              {/* <Image src='/logo.png' /> Log-in to your account */}
+            <div>
+              <Header as="h2" icon textAlign="center">
+                <Icon name="student" circular />
+                <Header.Content>teachersPet</Header.Content>
+              </Header>
+            </div>
+            <Header as="h2" color="teal" textAlign="center">
+               Log into your account
             </Header>
-            <Form size='large'>
+            <Form size="large">
               <Segment stacked>
-                <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
                 <Form.Input
                   fluid
-                  icon='lock'
-                  iconPosition='left'
-                  placeholder='Password'
-                  type='password'
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="E-mail address"
                 />
-    
-                <Button color='teal' fluid size='large'>
+                <Form.Input
+                  fluid
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Password"
+                  type="password"
+                />
+
+                <Button color="teal" fluid size="large">
                   Login
                 </Button>
               </Segment>
             </Form>
             <Message>
-              New to us? <a href='#'>Sign Up</a>
+            Once your admin has verified your email, create an account <a href="#">here</a>!
             </Message>
           </Grid.Column>
         </Grid>
-            {/* <Input placeholder='Search...' onChange={this.changeInput}/>
+        {/* <Input placeholder='Search...' onChange={this.changeInput}/>
             <Button onClick={this.changeDisplayed}/>
             <h2> hi {this.state.displayed}</h2> */}
-        </div>    
-      )
-    }
+      </div>
+    );
   }
-
+}
 
 export default Login;
-
-{/* <style>{`
-          body > div,
-          body > div > div,
-          body > div > div > div.login-form {
-            height: 100%;
-          }
-        `}
-        </style> */}
