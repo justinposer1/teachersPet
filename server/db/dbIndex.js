ab5b35801a3846396e24aa47a96e3f031efd44ff
justinposer <jposer1@gmail.com>
2019-06-14 16:30:18 -0400

    complexity discovered, but it is not intractable

--- server/db/dbIndex.js
+++ server/db/dbIndex.js
@@ -1,15 +1,34 @@
 
-const pg = require('pg');
+const pg = require("pg");
 
 const pool = new pg.Pool({
-    user: 'teacherspet',
-    host: '127.0.0.1',
-    database: 'teacherspetdata',
-    password: process.env.pgPassword,
-    port: '5432'
+  user: "teacherspet",
+  host: "127.0.0.1",
+  database: "postgres",
+  password: process.env.pgPassword,
+  port: "5432"
 });
 
-pool.query('CREATE TABLE users(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL,lastName VARCHAR(40) NOT NULL)', (err, res) => {
-    console.log(err, res);
-    pool.end();
+let name = "exampleschool";
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error("Error acquiring client", err.stack);
+  }
+  client.query(`CREATE DATABASE ${name}data`, (err, result) => {
+    client.query(`CREATE TABLE example(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL,lastName VARCHAR(40) NOT NULL)`, (err, result) => {
+    
+        
+        if (err) {
+          return console.error("Error executing query", err.stack);
+        }
+      });
+
+    release();
+    if (err) {
+      return console.error("Error executing query", err.stack);
+    }
+  });
 });
+
+
