f0d916c3031834ec2016b201930bad535a0f9a00
justinposer <jposer1@gmail.com>
2019-06-14 18:01:00 -0400

    got the double pool function to work

--- server/db/dbIndex.js
+++ server/db/dbIndex.js
@@ -16,19 +16,36 @@ pool.connect((err, client, release) => {
     return console.error("Error acquiring client", err.stack);
   }
   client.query(`CREATE DATABASE ${name}data`, (err, result) => {
-    client.query(`CREATE TABLE example(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL,lastName VARCHAR(40) NOT NULL)`, (err, result) => {
-    
-        
-        if (err) {
-          return console.error("Error executing query", err.stack);
-        }
-      });
 
     release();
     if (err) {
-      return console.error("Error executing query", err.stack);
+      return console.error("Error creating database", err.stack);
+    } else {
+        createTables();
     }
   });
 });
 
+const createTables = () => {
 
+    const newPool = new pg.Pool({
+        user: "teacherspet",
+        host: "127.0.0.1",
+        database: `${name}data`,
+        password: process.env.pgPassword,
+        port: "5432"
+      });
+
+      newPool.connect((err, client, release) => {
+        if (err) {
+          return console.error("Error acquiring client", err.stack);
+        }
+        client.query('CREATE TABLE exampletable(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL,lastName VARCHAR(40) NOT NULL)', (err, result) => {
+      
+          release();
+          if (err) {
+            return console.error("Error creating database", err.stack);
+          }
+        });
+      });
+}
