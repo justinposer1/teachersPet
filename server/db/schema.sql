659aa13f14b0f5f5bf770dc43f56ecaa981ee327
justinposer <jposer1@gmail.com>
2019-06-24 11:34:07 -0400

    added references

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -8,13 +8,13 @@ CREATE TABLE staff (
   hashedPassword varchar(50) NOT NULL,
   firstJoined date NOT NULL,
   latestDeparture date,
-  PRIMARY KEY (ID)
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE students (
   name varchar(50) NOT NULL,
   pictureFile varchar(50),
-  grade_id integer FOREIGN KEY,
+  grade_id integer FOREIGN KEY references gradeLevels (id),
   email varchar(50),
   homeless boolean,
   inFosterCare boolean,
@@ -22,56 +22,57 @@ CREATE TABLE students (
   firstJoined date NOT NULL,
   latestDeparture date,
   IEPLink varchar(50),
-  PRIMARY KEY (ID)
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE courses (
   name varchar(50) NOT NULL,
-  grade_id integer FOREIGN KEY,
+  grade_id integer FOREIGN KEY references (id),
   credits decimal (3, 2) NOT NULL,
   honors boolean,
-  deparment_id integer FOREIGN KEY,
-  semester_id integer FOREIGN KEY,
-  PRIMARY KEY (ID)
+  deparment_id integer FOREIGN KEY references (id),
+  semester_id integer FOREIGN KEY references (id),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE gradeLevels (
   level integer NOT NULL,
-  admin_id integer FOREIGN KEY,
-  PRIMARY KEY (ID)
+  admin_id integer FOREIGN KEY references (id),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE deparments (
   name varchar(50) NOT NULL,
-  admin_id integer FOREIGN KEY,
-  PRIMARY KEY (ID)
+  admin_id integer FOREIGN KEY references (id),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE semesters (
   name varchar(50) NOT NULL,
-  admin_id integer FOREIGN KEY,
-  PRIMARY KEY (ID)
+  admin_id integer FOREIGN KEY references (id),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE notes (
   name varchar(50) NOT NULL,
   text varchar(500),
-  staff_id integer FOREIGN KEY,
-  student_id integer FOREIGN KEY,
+  staff_id integer FOREIGN KEY references (id),
+  student_id integer FOREIGN KEY references (id),
   private boolean,
   flagged boolean,
   archived boolean,
-  PRIMARY KEY (ID)
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE staff_gradeLevels (
-  staff_id integer FOREIGN KEY,
-  gradeLevel_id integer FOREIGN KEY,
-  PRIMARY KEY (ID)
+  staff_id integer FOREIGN KEY references (id),
+  gradeLevel_id integer FOREIGN KEY references (id),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE staff_courses (
-  staff_id integer FOREIGN KEY,
-  course_id integer FOREIGN KEY,
-  PRIMARY KEY (ID)
+  staff_id integer FOREIGN KEY references (id),
+  course_id integer FOREIGN KEY references (id),
+  PRIMARY KEY (id)
 );
+
